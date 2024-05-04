7cf81ce62c98c5ffac5d119c318b889405f8a690
Ionizing <PeterSmith_9@outlook.com>
2024-05-04 18:04:40 +0800

    [Main.hs] wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,44 @@
 module Main where
 
+import Data.List (intercalate)
+import Text.Printf (printf)
 import Options.Applicative
---import Text.Parsec
---import Text.Printf
 import Library
 
 
-data App = App [Quantity]
+data App = App [Quantity] String
     deriving (Eq, Show)
 
 app :: Parser App
 app = App
     <$> some (argument auto (
-        metavar "QUANTITIES"
-     <> help "E.g. '1.0eV', '20THz' and '3.14Kcm-1'..."
+        metavar "QUANTITIES" <> help "E.g. '1.0eV', '20THz' and '3.14Kcm-1'..."
     ))
+    <*> option str (
+        metavar "FORMAT" <> long "fmt" <> short 'f' <> value "%10.5q"
+    <>  help "Format the output. Strings like '%10.5q' and '%10.5q' are available."
+    )
+
+
+formatQuantities :: String -> Quantity -> String
+formatQuantities fmt q = originalStr ++ "  == " ++ formattedStr
+    where
+    originalStr  = printf fmt q
+    formattedStr = intercalate " | " $ map (printf fmt) converted
+    converted    = map convertQuantity [
+          ElectronVolt
+        , CaloriePerMole
+        , JoulePerMole
+        , Kelvin
+        , Hartree
+        , Wavenumber
+        , Meter
+        , Hertz
+        ] <*> pure q
 
 
 run :: App -> IO ()
-run (App xs) = print xs
+run (App xs fmt) = putStrLn $ intercalate "\n" $ map (formatQuantities fmt) xs
 
 
 main :: IO ()
@@ -33,10 +53,10 @@ main = run =<< execParser opts
         <> header "Unit conversion toy for quantum chemistry researchers."
         <> footer (
             "Available units: 'eV Cal/mol J/mol K(Kelvin) Ha(Hartree) cm-1 m(meter) Hz'."
-        ++  " Prefixes from 'a(Atto)' to 'E(Exa)' are also available."
+        ++  " Prefixes from 'a'(Atto) to 'E'(Exa) are also available."
         ++  " In order to eliminate the confusions,"
-        ++  " the prefixes smaller than unity (from 'a(Atto)' to 'm(Milli-)') should be lowercased,"
-        ++  " while the larger prefixes (from 'K(Kilo)' to 'E(Exa)') should be uppercased."
+        ++  " the prefixes smaller than unity (from 'a'(Atto) to 'm'(Milli)) should be lowercased,"
+        ++  " while the larger prefixes (from 'K'(Kilo) to 'E'(Exa)) should be uppercased."
         ++  " For example: '1meV' means '1 Milli ElectronVolt' while '1MeV' stands for '1 Mega ElectronVolt'"
         )
         )
